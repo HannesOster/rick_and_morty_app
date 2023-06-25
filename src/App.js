acd8ff4f7f4bd819052d800a951b626a891864c1
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-06-25 12:05:34 +0200

    add structure

--- src/App.js
+++ src/App.js
@@ -19,21 +19,39 @@ function App() {
       <main>
         <SearchBar />
         <CardContainer>
-          <Card card={cards[0]} />
-          <Card card={cards[1]} />
-          <Card card={cards[2]} />
-          <Card card={cards[3]} />
-          <Card card={cards[4]} />
-          <Card card={cards[0]} />
-          <Card card={cards[0]} />
-          <Card card={cards[0]} />
-          <Card card={cards[0]} />
-          <Card card={cards[0]} />
+          <GetCardElements cards={cards} />
         </CardContainer>
       </main>
       <Navigation />
     </div>
   );
+}
+function GetCardElements({ cards }) {
+  cards.forEach((card) => {
+    return (
+      <li className="card">
+        <div className="card__image-container">
+          <img className="card__image" src={card.image} alt={card.name} />
+          <div className="card__image-gradient"></div>
+        </div>
+        <div className="card__content">
+          <h2 className="card__title">{card.name}</h2>
+          <dl className="card__info">
+            <dt className="card__info-title">Status</dt>
+            <dd className="card__info-description">{card.status}</dd>
+            <dt className="card__info-title">Species</dt>
+            <dd className="card__info-description">{card.species}</dd>
+            <dt className="card__info-title">Type</dt>
+            <dd className="card__info-description">{card.type}</dd>
+            <dt className="card__info-title"></dt>
+            <dd className="card__info-description">{card.type}</dd>
+            <dt className="card__info-title">Occurrences</dt>
+            <dd className="card__info-description">{card.episode.length}</dd>
+          </dl>
+        </div>
+      </li>
+    );
+  });
 }
 
 export default App;
