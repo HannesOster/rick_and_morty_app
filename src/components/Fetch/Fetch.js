85dfff73d6d918bacbdb60ef6e58119f3f5fcb0b
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-06-28 09:30:32 +0200

    add state and useeffect

--- src/components/Fetch/Fetch.js
+++ src/components/Fetch/Fetch.js
@@ -1,5 +1,7 @@
-export default async function fetchData() {
-  const response = await fetch("https://rickandmortyapi.com/api/character");
+export default async function fetchData(name) {
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character?name=${name}`
+  );
   const data = await response.json();
   console.log(data);
   return data;
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,14 +1,13 @@
-export default function SearchBar() {
+export default function SearchBar({ setName }) {
   return (
-    <>
-      <ul className="search__list">
-        <li>
-          <label htmlFor="searchbar">Search for Characters</label>
-        </li>
-        <li>
-          <input className="search__input" id="searchbar" />
-        </li>
-      </ul>
-    </>
+    <div className="search__wrapper">
+      <label htmlFor="searchbar">Search for Characters</label>
+
+      <input
+        onChange={(event) => setName(event.target.value)}
+        className="search__input"
+        id="searchbar"
+      />
+    </div>
   );
 }
